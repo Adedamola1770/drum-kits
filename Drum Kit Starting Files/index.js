f4e9d5fbc9399cf52995e19fcc6ffcf07669b917
Adedamola1770 <damola.adeneye@gmail.com>
2024-06-04 16:03:21 +0100

    first commit

---
+++ Drum Kit Starting Files/index.js
@@ -0,0 +1,79 @@
+// alert('welcome on board');
+// document.querySelector('button').addEventListener('click', handleClick)
+// document.querySelectorAll('.set').addEventListener('click', handleClick)
+// document.getElementsByClassName('.set').addEventListener('click', handleClick)
+
+
+
+function handleClick() {
+alert('I just got clicked');
+}
+
+var numberOfButtons = document.querySelectorAll('.drum').length;
+
+for (var i = 0; i < numberOfButtons; i++) {
+    document.querySelectorAll('.drum')[i].addEventListener('click', function () {
+        // alert('I got Clicked');
+
+        var buttonInnerHTML = this.innerHTML;
+        switch (buttonInnerHTML) {
+            case 'k':
+                var tom1 = new Audio('sounds/tom-1.mp3');
+                tom1.play();
+                break;
+
+                case 'a':
+                    var kick = new Audio('sounds/kick-bass.mp3');
+                kick.play();
+                break;
+                // var tom2 = new Audio('sounds/tom-2.mp3');
+                // tom2.play();
+                // break;
+
+                case 'j':
+                var tom3 = new Audio('sounds/tom-3.mp3');
+                tom3.play();
+                break;
+
+                case 'd':
+                var tom4 = new Audio('sounds/tom-4.mp3');
+                tom4.play();
+                break;
+
+                case 's':
+                var snare = new Audio('sounds/snare.mp3');
+                snare.play();
+                break;
+
+                case 'w':
+                var crash = new Audio('sounds/crash.mp3');
+                crash.play();
+                break;
+
+                case 'l':
+                    var tom2 = new Audio('sounds/tom-2.mp3');
+                tom2.play(); 
+                break;
+                
+
+            default: console.log(buttonInnerHTML);
+                
+        }
+
+        
+        });
+
+    }
+     
+    
+
+
+
+
+
+
+
+
+
+  
+
